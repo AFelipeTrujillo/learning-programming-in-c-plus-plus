0556a85be36e711500a21c0dcb9573dcb579d684
Andrés Felipe Trujillo <andres.trujillo.1@gmail.com>
2026-02-12 16:46:36 +0100

    POO with C++

---
+++ 02_poo/bank.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+class BankAccount {
+    private:
+        double balance;
+    
+    public:
+        
+        /*
+        BankAccount(double initial_balance) {
+            this->balance = initial_balance;
+        }
+        */
+
+        // The constructor can also be defined using an initializer list, 
+        // which is a more efficient way to initialize member variables.
+        BankAccount(double initial_balance) : balance(initial_balance) {
+
+        }
+
+        void deposit(double amount) {
+            if (amount > 0 ) {
+                this->balance += amount;
+            }
+        }
+
+        void withdraw(double amount) {
+            if(amount >0 && amount <= this->balance) {
+                this->balance -= amount;
+            }
+        }
+
+        // const member function: a member function that does not modify the state of the object.
+        void getBalance() const { 
+            std::cout << "Current balance: " << this->balance << std::endl; 
+        }
+};
+
+int main () {
+    BankAccount myAccount(100);
+    myAccount.getBalance();
+    myAccount.deposit(50); 
+    myAccount.getBalance();
+    myAccount.withdraw(30); 
+    myAccount.getBalance();
+    return 0;
+}
