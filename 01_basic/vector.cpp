c81c6bc2bd3709d51e1064064556d48e2ad8e569
Andrés Felipe Trujillo <andres.trujillo.1@gmail.com>
2026-02-12 16:27:25 +0100

    vectors

--- 01_basic/vector.cpp
+++ 01_basic/vector.cpp
@@ -16,8 +16,19 @@ void show_vector(const std::vector<int>& vec) {
     std::cout << std::endl;
 }
 
+void show_vector_with_for_loop(const std::vector<int>& vec) { 
+    
+    for (const int &element : vec) { 
+        std::cout << element << " "; 
+    } 
+    std::cout << std::endl;
+}
+
 int main() {
     std::vector <int> inventory = {10, 20, 30, 40, 50};
     std::cout << "Original inventory: "; show_vector(inventory); multiply_by_two(inventory); std::cout << "Updated inventory: "; show_vector(inventory);
+    multiply_by_two(inventory);
+    std::cout << "For Loop: "; show_vector_with_for_loop(inventory); 
+    std::cout << std::endl;
     return 0;
 }
